8fe45a889e022f93353ceb1acbcec86cc0563906
nick <nick.twort@houstonkemp.com>
2022-05-06 15:01:14 +1000

    Pull out refresh into function and only use buttons when relevant

--- app.R
+++ app.R
@@ -1,6 +1,44 @@
 
 library(shiny)
 library(spotifyr)
+library(httr)
+library(jsonlite)
+
+get_now_playing <- function() {
+  authorization <- get_spotify_authorization_code()
+  base_url <- "https://api.spotify.com/v1/me/player/currently-playing"
+  params <- list(market = NULL)
+  res <- RETRY("GET", base_url, config(token = authorization), 
+               query = params, encode = "json")
+  stop_for_status(res)
+  
+  if (res$status_code == 204) {
+    res <- list(
+      item = list(
+        name = "Nothing playing",
+        artists = list(
+          name = "",
+          name1 = NA,
+          name2 = NA
+        ),
+        album = list(
+          name = "",
+          images = list(
+            url1 = ""
+          )
+        ),
+        is_playing = "FALSE"
+      )
+    )
+  } else {
+    res <- fromJSON(content(res, as = "text", encoding = "UTF-8"), 
+                    flatten = TRUE)
+  }
+  
+  unlist(res)[c("item.name", "item.artists.name", "item.artists.name1", "item.artists.name2", "item.album.name", "item.album.images.url1", "is_playing")]
+
+}
+
 
 # Define UI for application
 ui <- fillPage(
@@ -23,23 +61,21 @@ ui <- fillPage(
 server <- function(input, output, session) {
   
   x <- reactivePoll(
-    intervalMillis = 5000,
+    intervalMillis = 3000,
     session = session, 
     checkFunc = Sys.time,
-    valueFunc = function() {
-      unlist(get_my_current_playback())[c("item.name", "item.artists.name", "item.artists.name1", "item.artists.name2", "item.album.name", "item.album.images.url1")]
-    })
+    valueFunc = get_now_playing)
   
   
   output$album_art <- renderText({c('<img width="40%" src="',x()[["item.album.images.url1"]],'">')})
   output$track_title <- renderText(x()[["item.name"]])
   output$artist <- renderText(paste0(na.omit(x()[c("item.artists.name", "item.artists.name1", "item.artists.name2")]), collapse = ", "))
   output$album <- renderText(x()[["item.album.name"]])
-  
-  observeEvent(input$previ, {try(skip_my_playback())})
-  observeEvent(input$start, {try(start_my_playback())})
-  observeEvent(input$pause, {try(pause_my_playback())})
-  observeEvent(input$nexti, {try(skip_my_playback_previous())})
+
+  observeEvent(input$previ, {try(skip_my_playback_previous())})
+  observeEvent(input$start, {try(if(x()[["is_playing"]] == "FALSE") start_my_playback())})
+  observeEvent(input$pause, {try(if(x()[["is_playing"]] == "TRUE") pause_my_playback())})
+  observeEvent(input$nexti, {try(skip_my_playback())})
   
 }
 
